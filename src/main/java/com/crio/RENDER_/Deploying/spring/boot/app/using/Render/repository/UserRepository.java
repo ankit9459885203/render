0d06e9ca07f73ecd3ad4a2b1750cc54e3f581696
Ankit <ankit9459885203@gmail.com>
2025-06-21 16:38:27 +0530

    Update UserRepository and remove unused file

--- src/main/java/com/crio/RENDER_/Deploying/spring/boot/app/using/Render/repository/UserRepository.java
+++ src/main/java/com/crio/RENDER_/Deploying/spring/boot/app/using/Render/repository/UserRepository.java
@@ -10,3 +10,13 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface UserRepository extends JpaRepository<User, Long> {
     // Spring Data REST will automatically expose CRUD endpoints for the User entity.
 }
+
+
+/*The @RepositoryRestResource annotation automatically exposes your UserRepository as RESTful endpoints with:
+
+path = "users" Defines the URL path for the REST endpoints.   Example:  GET /users → Returns all users    ,POST /users → Creates a new user
+collectionResourceRel = "users"   Defines the JSON wrapper key for the response.
+
+
+Hence No need to write controllers—Spring Data REST handles it automatically.
+*/
